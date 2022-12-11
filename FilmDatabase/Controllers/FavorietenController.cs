999091cc52f0e5a32199b5131787391f810ad901
GuyJanssen33 <r0237357@student.thomasmore.be>
2022-12-11 16:50:21 +0100

    views toegevoegd

---
+++ FilmDatabase/Controllers/FavorietenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDatabase.Controllers
+{
+	public class FavorietenController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ FilmDatabase/Controllers/FilmToevoegenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDatabase.Controllers
+{
+	public class FilmToevoegenController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
