863e37c1c8008ffaff2aa7a369dd0ecdbb030d02
GuyJanssen33 <r0237357@student.thomasmore.be>
2022-12-27 16:45:20 +0100

    controllers toegevoegd

---
+++ FilmDatabase/Controllers/ProducentToevoegenController.cs
@@ -0,0 +1,53 @@
+﻿using FilmDatabase.Data.UnitOfWork;
+using FilmDatabase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDatabase.Controllers
+{
+	public class ProducentToevoegenController : Controller
+	{
+
+		private readonly ILogger<ActeurToevoegenController> _logger;
+
+		private readonly IUnitOfWork _uow;
+
+		public ProducentToevoegenController(ILogger<ActeurToevoegenController> logger, IUnitOfWork uow)
+		{
+			_logger = logger;
+			_uow = uow;
+		}
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		//public IActionResult Index(int id)
+		//{
+		//	return View();
+		//}
+
+		[HttpPost]
+		public async Task<ActionResult<Producent>> AddProducent(Producent Producenta, FilmProducent filmProducenta, int id)
+		{
+			FilmProducent producentFilm = new FilmProducent();
+			_uow.ProducentRepository.Create(Producenta);
+			await _uow.Save();
+			List<Film> films = _uow.FilmRepository.GetAll().ToList();
+			Film film = films.Last();
+			List<Producent> producenten = _uow.ProducentRepository.GetAll().ToList();
+			Producent producent = producenten.Last();
+
+			producentFilm.FilmId = film.FilmId;
+			producentFilm.ProducentId = producent.ProducentId;
+			_uow.FilmProducentRepository.Create(producentFilm);
+			await _uow.Save();
+			return RedirectToAction("Index", "Regisseur", new { area = "" });
+		}
+	}
+}
---
+++ FilmDatabase/Controllers/RegisseurToevoegenController.cs
@@ -0,0 +1,48 @@
+﻿using FilmDatabase.Data.UnitOfWork;
+using FilmDatabase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDatabase.Controllers
+{
+	public class RegisseurToevoegenController : Controller
+	{
+
+		private readonly ILogger<RegisseurToevoegenController> _logger;
+
+		private readonly IUnitOfWork _uow;
+
+
+
+		public RegisseurToevoegenController(ILogger<RegisseurToevoegenController> logger, IUnitOfWork uow)
+		{
+			_logger = logger;
+			_uow = uow;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Regisseur>> AddRegisseur(Regisseur Regisseura, FilmProducent filmProducenta)
+		{
+			FilmRegisseur regisseurFilm = new FilmRegisseur();
+			_uow.RegisseurRepository.Create(Regisseura);
+			await _uow.Save();
+			List<Film> films = _uow.FilmRepository.GetAll().ToList();
+			Film film = films.Last();
+			List<Regisseur> regisseurs = _uow.RegisseurRepository.GetAll().ToList();
+			Regisseur regisseur = regisseurs.Last();
+
+			regisseurFilm.FilmId = film.FilmId;
+			regisseurFilm.RegisseurId = regisseur.RegisseurId;
+			_uow.FilmRegisseurRepository.Create(regisseurFilm);
+			await _uow.Save();
+			return RedirectToAction("Index", "Home", new { area = "" });
+		}
+	}
+}
